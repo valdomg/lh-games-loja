5e30e6df2d809fed6a67bcc221a22da52b934a80
valdom <valdom221@gmail.om>
2024-02-27 11:50:05 -0300

    Changes in layout

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -8,6 +8,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-inicio',
@@ -21,7 +22,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatInputModule,
     MatMenuModule,
     MatToolbarModule,
-
+    MatGridListModule
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
